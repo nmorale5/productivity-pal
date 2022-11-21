3bb62eb48e671c67b92adee0f3b0de50fc505067
nmorale5 <nmorale5@mit.edu>
2022-11-20 20:52:00 -0500

    chart.js infrastructure

--- Frontend/analytics/test.js
+++
@@ -1,8 +0,0 @@
-async function retrieveData() {
-    let { allData } = await chrome.storage.local.get("allData")
-    for (url of Object.entries(allData)) {
-        console.log(url, allData[url])
-    }
-}
-
-retrieveData()
